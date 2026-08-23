b74897dd2906e3ebf015d89b33133bf29325359b
hilmanauzn <hilman.auzan@goersapp.com>
2026-08-23 20:45:37 +0700

    fix: layout scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen bg-gray-50">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
-        <main className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        <main className="flex-1 flex flex-col min-h-0 overflow-hidden">
           <MenuSection />
         </main>
-        <aside className="hidden md:flex flex-col w-96 border-l border-gray-200">
+        <aside className="hidden md:flex flex-col w-96 border-l border-gray-200 min-h-0">
           <Cart />
         </aside>
       </div>
